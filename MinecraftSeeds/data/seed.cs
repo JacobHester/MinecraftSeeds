55025534f97e219577ff39ba0e0a30ce6cb1ca94
Justen N <losthalflingrogue@gmail.com>
2019-07-16 11:10:11 -0600

    database change

---
+++ MinecraftSeeds/data/seed.cs
@@ -0,0 +1,15 @@
+namespace MinecraftSeeds.data
+{
+    public class seed
+    {
+        public int SeedID { get; set; }
+
+        public string SeedValue { get; set; }
+        public string SeedText {get; set;}
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+        public string version { get; set; }
+    }
+}
