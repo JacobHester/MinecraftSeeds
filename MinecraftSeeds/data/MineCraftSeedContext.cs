1aca2782a90c10943c9161adb565555e907e9106
Justen N <losthalflingrogue@gmail.com>
2019-07-17 17:08:56 -0600

    merge again

--- MinecraftSeeds/data/MineCraftSeedContext.cs
+++ MinecraftSeeds/data/MineCraftSeedContext.cs
@@ -10,12 +10,7 @@ namespace MinecraftSeeds.data
 
         }
               public DbSet<seed> Seeds {get; set;}
-<<<<<<< HEAD
-
-              public DbSet<image> Images { get; set; }
-=======
               
-              public DbSet<Image> Images {get; set;}
->>>>>>> a0390d3ce27dfd39ee8e6acc0b870faff5fa1299
+              public DbSet<image> Images {get; set;}
     }
 }
--- MinecraftSeeds/data/image.cs
+++ MinecraftSeeds/data/image.cs
@@ -1,10 +1,6 @@
 namespace MinecraftSeeds.data
 {
-<<<<<<< HEAD
     public class image
-=======
-    public class Image
->>>>>>> a0390d3ce27dfd39ee8e6acc0b870faff5fa1299
     {
         public int ImageId { get; set; }
 
